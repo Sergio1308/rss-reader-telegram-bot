5a24b8f80a3f0f3b29f6fa0e8fd7e911daadf1ad
Sergei <sergi.haiduk@gmail.com>
2023-03-24 06:04:20 +0200

    removed some constants

--- src/main/java/com/company/RssReaderBot/handlers/CallbackVars.java
+++ src/main/java/com/company/RssReaderBot/handlers/CallbackVars.java
@@ -15,8 +15,6 @@ public class CallbackVars {
     public static final String UPDATE_USER_DATA = "update_user_data";
     public static final String RETURN_LOAD_BY_TITLE = "load_by_title_inline";
     public static final String SELECTED_BY_TITLE_CALLBACK = "EP "; // custom marker
-    public static final String ITEM_DESCRIPTION = "get_description";
-    public static final String ITEM_OTHER_DETAILS = "get_other_details";
     public static final String NEXT_PAGE = "next_page";
     public static final String PREVIOUS_PAGE = "previous_page";
     public static final String FIRST_PAGE = "first_page";
