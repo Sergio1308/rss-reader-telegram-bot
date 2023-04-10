2afedee68dd2e86ebb2da3e2f0671d73b93e062e
Sergei <sergi.haiduk@gmail.com>
2023-04-10 03:12:22 +0300

    removed unnecessary classes

--- src/main/java/com/company/RssReaderBot/handlers/Handler.java
+++
@@ -1,8 +0,0 @@
-package com.company.RssReaderBot.handlers;
-
-import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.BaseRequest;
-
-public interface Handler {
-    BaseRequest handle(Update update);
-}
