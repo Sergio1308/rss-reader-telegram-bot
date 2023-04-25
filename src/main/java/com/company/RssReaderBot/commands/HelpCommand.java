6af3db3ed6871b5c13dada7726d003e6a9055c36
Sergei <sergi.haiduk@gmail.com>
2023-04-25 22:32:20 +0300

    sendMessage text and method argument changed

--- src/main/java/com/company/RssReaderBot/commands/HelpCommand.java
+++ src/main/java/com/company/RssReaderBot/commands/HelpCommand.java
@@ -2,26 +2,25 @@ package com.company.RssReaderBot.commands;
 
 import com.company.RssReaderBot.inlinekeyboard.InlineKeyboardCreator;
 import com.company.RssReaderBot.inlinekeyboard.HelpCommandInlineKeyboard;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.BaseRequest;
-import com.pengrad.telegrambot.request.EditMessageText;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.BaseResponse;
-import com.pengrad.telegrambot.response.SendResponse;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HelpCommand implements Command<Long, Integer> {
+public class HelpCommand implements Command<Message> {
 
     private String getAnswer() {
-        return "Here you can read about what an RSS feed is, see and use feed examples." +
-                "\nSome of the buttons are clickable and will redirect you to the page in the browser";
+        return "Welcome to the help menu. Here, you can read about what an RSS feed is, see and use feed examples. " +
+                "Also, you can use the first button to subscribe to the sample feed." +
+                "\nSome of the buttons are clickable and will redirect you to the page in the browser.";
     }
 
     @Override
-    public BaseRequest<SendMessage, SendResponse> execute(Long chatId, Integer messageId) {
+    public BaseRequest<?, ?> execute(Message message) {
         InlineKeyboardCreator inlineKeyboardCreator = new HelpCommandInlineKeyboard();
         InlineKeyboardMarkup markupInline = inlineKeyboardCreator.createInlineKeyboard();
-        return new SendMessage(chatId, getAnswer()).replyMarkup(markupInline);
+        return new SendMessage(message.chat().id(), getAnswer()).replyMarkup(markupInline);
     }
 }
