3a72c08b313a3a0139c70c0c16e1a97333c9d90d
Sergei <sergi.haiduk@gmail.com>
2023-04-26 02:21:45 +0300

    added more callback constants

--- src/main/java/com/company/RssReaderBot/controllers/CallbackDataConstants.java
+++ src/main/java/com/company/RssReaderBot/controllers/CallbackDataConstants.java
@@ -1,21 +1,18 @@
 package com.company.RssReaderBot.controllers;
 
-public class CallbackQueryConstants {
+public class CallbackDataConstants {
     // callback data fields
     public static final String START_MENU = "/start";
     public static final String SUBSCRIBE = "subscribe";
     public static final String UNSUBSCRIBE = "unsubscribe";
-    public static final String SUB_FEED_SAMPLE = "https://rss.nytimes.com/services/xml/rss/nyt/Sports.xml";
+    public static final String SUB_FEED_SAMPLE = "https://rss.nytimes.com/services/xml/rss/nyt/Sports.xml"; // todo
     public static final String FEED_BUTTON = "feed_";
     public static final String PARSING_ELEMENTS_MENU = "main_menu";
     public static final String LOAD_ALL_ITEMS = "load_all_items";
     public static final String LOAD_BY_TITLE = "load_by_title";
     public static final String LOAD_BY_DATE = "load_by_date";
-    public static final String SETTINGS = "personal_menu";
+    public static final String SETTINGS = "settings_menu";
     public static final String FAVORITES = "user_favorites";
-    public static final String CHANGE_RSS_URL = "change_rss_url";
-    public static final String ALERTS_AND_TITLE_CONF = "alerts_title_items";
-    public static final String UPDATE_USER_DATA = "update_user_data";
     public static final String RETURN_LOAD_BY_TITLE = "load_by_title_inline";
     public static final String SHOW_ITEM = "show_item_";
     public static final String NEXT_PAGE = "next_page";
