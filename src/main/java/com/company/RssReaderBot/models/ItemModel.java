a8dfff93f795db2a83ba0cd3cf6a51756db66604
Sergei <sergi.haiduk@gmail.com>
2023-05-12 23:07:27 +0300

    implemented builder

--- src/main/java/com/company/RssReaderBot/models/ItemModel.java
+++ src/main/java/com/company/RssReaderBot/models/ItemModel.java
@@ -1,43 +1,18 @@
 package com.company.RssReaderBot.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
+@Getter
+@Builder
+@ToString
 public class ItemModel {
 
-    @Getter
     private final String title;
-    @Getter @Setter
     private String description;
-    @Getter @Setter
     private Date pubDate;
-    @Getter @Setter
     private String mediaUrl;
-    @Getter @Setter
-    private String guid;
+    private String sourceLink;
 
-    public ItemModel(String title, String description, Date pubDate, String mediaUrl, String guid) {
-        this.title = title;
-        this.description = description;
-        this.pubDate = pubDate;
-        this.mediaUrl = mediaUrl;
-        this.guid = guid;
-    }
-
-    public ItemModel(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "ItemModel{" +
-                "title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", pubDate='" + pubDate + '\'' +
-                ", mediaUrl='" + mediaUrl + '\'' +
-                ", guid='" + guid + '\'' +
-                '}';
-    }
 }
