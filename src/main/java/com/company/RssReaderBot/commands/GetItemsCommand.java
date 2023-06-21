31e1c9674767fb6c37a1b43405f55cd35bd3302d
Sergei <sergi.haiduk@gmail.com>
2023-06-21 22:49:52 +0300

    implemented logic for displaying feeds list

--- src/main/java/com/company/RssReaderBot/commands/GetItemsCommand.java
+++ src/main/java/com/company/RssReaderBot/commands/GetItemsCommand.java
@@ -1,9 +1,9 @@
 package com.company.RssReaderBot.commands;
 
 import com.company.RssReaderBot.db.entities.RssFeed;
-import com.company.RssReaderBot.inlinekeyboard.InlineKeyboardCreator;
 import com.company.RssReaderBot.inlinekeyboard.GetItemsInlineKeyboard;
 import com.company.RssReaderBot.db.services.RssFeedService;
+import com.company.RssReaderBot.inlinekeyboard.RssFeedsInlineKeyboard;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.request.ForceReply;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
@@ -11,6 +11,8 @@ import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.EditMessageText;
 import com.pengrad.telegrambot.request.SendMessage;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -20,22 +22,31 @@ public class GetItemsCommand implements Command<Message> {
 
     private final RssFeedService rssFeedService;
 
-    public GetItemsCommand(RssFeedService rssFeedService) {
+    private final RssFeedsInlineKeyboard feedsInlineKeyboard;
+
+    private final GetItemsInlineKeyboard getItemsInlineKeyboard;
+
+    @Getter @Setter
+    private List<RssFeed> feedList;
+
+    public GetItemsCommand(RssFeedService rssFeedService, RssFeedsInlineKeyboard feedsInlineKeyboard,
+                           GetItemsInlineKeyboard getItemsInlineKeyboard) {
         this.rssFeedService = rssFeedService;
+        this.feedsInlineKeyboard = feedsInlineKeyboard;
+        this.getItemsInlineKeyboard = getItemsInlineKeyboard;
     }
 
     private String getAnswer() {
-        return "I found some subscribed feeds, select one by pressing the button, " +
-                "then press other option button below to get items: all, by title, date OR" +
-                "\nif you want get items from another feed use command <code>/get</code> with one of this arguments:" +
-                "\nall|title|date, then write URL of your feed. For example, " +
-                "<code>/get title https://example.com/rss.xml</code> - if you want to get items by title";
+        return "I found some subscribed feeds!" +
+                "\n\nSelect one by pressing scroll buttons◀️▶️, " +
+                "then press other option button below to get items: all, by title or by date" +
+                "\n\nSelected feed:";
     }
 
     @Override
     public BaseRequest<?, ?> execute(Message message) {
         long chatId = message.chat().id();
-        List<RssFeed> feedList = rssFeedService.getAllFeeds(chatId);
+        feedList = rssFeedService.getAllFeeds(chatId);
         if (feedList.isEmpty()) {
             return new SendMessage(
                     chatId,
@@ -44,8 +55,10 @@ public class GetItemsCommand implements Command<Message> {
                     .parseMode(ParseMode.HTML)
                     .replyMarkup(new ForceReply(true).inputFieldPlaceholder("all|title|date url"));
         }
-        InlineKeyboardCreator inlineKeyboardCreator = new GetItemsInlineKeyboard(feedList);
-        InlineKeyboardMarkup markupInline = inlineKeyboardCreator.createInlineKeyboard();
-        return new EditMessageText(chatId, message.messageId(), getAnswer()).replyMarkup(markupInline).parseMode(ParseMode.HTML);
+        feedsInlineKeyboard.setFeedList(feedList);
+        InlineKeyboardMarkup markupInline = getItemsInlineKeyboard.createInlineKeyboard();
+        return new EditMessageText(chatId, message.messageId(), getAnswer())
+                .replyMarkup(markupInline)
+                .parseMode(ParseMode.HTML);
     }
 }
--- src/main/java/com/company/RssReaderBot/inlinekeyboard/GetItemsInlineKeyboard.java
+++ src/main/java/com/company/RssReaderBot/inlinekeyboard/GetItemsInlineKeyboard.java
@@ -1,28 +1,29 @@
 package com.company.RssReaderBot.inlinekeyboard;
 
 import com.company.RssReaderBot.controllers.CallbackDataConstants;
-import com.company.RssReaderBot.db.entities.RssFeed;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class GetItemsInlineKeyboard implements InlineKeyboardCreator {
 
-    private final List<RssFeed> feedList;
+    private final RssFeedsInlineKeyboard feedsInlineKeyboard;
 
-    public GetItemsInlineKeyboard(List<RssFeed> feedList) {
-        this.feedList = feedList;
+    public GetItemsInlineKeyboard(RssFeedsInlineKeyboard feedsInlineKeyboard) {
+        this.feedsInlineKeyboard = feedsInlineKeyboard;
     }
 
     @Override
     public InlineKeyboardMarkup createInlineKeyboard() {
-        RssFeedsInlineKeyboard feedsInlineKeyboard = new RssFeedsInlineKeyboard(feedList);
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
         InlineKeyboardButton[] feedsButtons = feedsInlineKeyboard.createFeedListButton();
+        markup.addRow(feedsButtons);
+        createGetItemsButtons(markup);
+        return markup;
+    }
 
+    public void createGetItemsButtons(InlineKeyboardMarkup inlineMarkup) {
         InlineKeyboardButton[] getItemsButtons = new InlineKeyboardButton[] {
                 new InlineKeyboardButton("Get all items")
                         .callbackData(CallbackDataConstants.LOAD_ALL_ITEMS),
@@ -31,10 +32,9 @@ public class GetItemsInlineKeyboard implements InlineKeyboardCreator {
                 new InlineKeyboardButton("Get items by date")
                         .callbackData(CallbackDataConstants.LOAD_BY_DATE)
         };
-        markup.addRow(feedsButtons).addRow(new InlineKeyboardButton(" ")
-                        .callbackData("null"))
-                .addRow(getItemsButtons).addRow(new InlineKeyboardButton("Back to main menu")
+        inlineMarkup
+                .addRow(getItemsButtons)
+                .addRow(new InlineKeyboardButton("Back to main menu")
                         .callbackData(CallbackDataConstants.START_MENU));
-        return markup;
     }
 }
