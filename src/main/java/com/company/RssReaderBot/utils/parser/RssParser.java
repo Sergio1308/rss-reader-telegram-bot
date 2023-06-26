343662ebc06d47e819bb8ab252f1f57ca268d606
Sergei <sergi.haiduk@gmail.com>
2023-06-26 21:43:57 +0300

    changed the logic of parsing elements by date, added throwing custom exceptions and more documentation

--- src/main/java/com/company/RssReaderBot/utils/parser/RssParser.java
+++ src/main/java/com/company/RssReaderBot/utils/parser/RssParser.java
@@ -2,6 +2,8 @@ package com.company.RssReaderBot.utils.parser;
 
 import com.company.RssReaderBot.models.ItemModel;
 import com.company.RssReaderBot.utils.DateUtils;
+import com.company.RssReaderBot.utils.InvalidPubDateException;
+import com.company.RssReaderBot.utils.InvalidUserEnteredDateException;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
@@ -15,6 +17,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.*;
 
 /**
@@ -46,25 +49,46 @@ public class RssParser {
     @Getter
     private NodeList nodeList;
 
-    public void parseRss(String url) {
+    /**
+     * Parses the RSS feed from the provided URL and retrieves the list of items.
+     *
+     * @param url                       URL of the RSS feed to parse.
+     * @throws RssParsingException      If an error occurs while parsing the RSS feed.
+     */
+    public void parseRss(String url) throws RssParsingException {
         try {
             Document doc = getDocument(url);
             nodeList = doc.getElementsByTagName(ITEM);
         } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
+            throw new RssParsingException("Error parsing RSS: ", e);
         }
     }
 
+    /**
+     * Parses the feed title from the provided URL.
+     *
+     * @param url URL of the RSS feed to parse.
+     */
     public void parseFeedTitle(String url) {
         try {
             Document doc = getDocument(url);
             Element channel = (Element) doc.getElementsByTagName(CHANNEL).item(0);
             feedTitle = getOptionalSubElementValue(channel, TITLE);
         } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
+            // logging: "Error parsing feed title: ", e.getMessage()
+            feedTitle = "Noname";
         }
     }
 
+    /**
+     * Retrieves an XML Document object from the specified URL.
+     *
+     * @param url                               URL of the XML document to retrieve and parse.
+     * @return                                  Parsed XML Document object.
+     * @throws ParserConfigurationException     If a DocumentBuilder cannot be created.
+     * @throws SAXException                     If there is an error parsing the XML.
+     * @throws IOException                      If an I/O error occurs while fetching or reading the XML.
+     */
     private Document getDocument(String url) throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document doc = builder.parse(url);
@@ -106,13 +130,23 @@ public class RssParser {
         return itemsList;
     }
 
-    public List<ItemModel> getElementListByDate(String dateString) {
+    public List<ItemModel> getElementListByDate(String dateString) throws InvalidUserEnteredDateException {
         List<ItemModel> elementList = new ArrayList<>();
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node = nodeList.item(i);
             Element element = (Element) node;
             String currentItemDate = getOptionalSubElementValue(element, PUB_DATE);
-            if (currentItemDate.contains(dateString)) {
+
+            LocalDate userDate = DateUtils.parseLocalDateFromString(dateString);
+            Date itemDate;
+            try {
+                itemDate = DateUtils.parseDateFromString(currentItemDate);
+            } catch (InvalidPubDateException e) {
+                // log
+                continue;
+            }
+
+            if (DateUtils.compareDates(userDate, itemDate)) {
                 ItemModel currentItemModel = createItem(element);
                 elementList.add(currentItemModel);
             }
@@ -126,13 +160,20 @@ public class RssParser {
      * If there is no media object (enclosure), media content and thumbnail are checked.
      * If there is no link, guid is checked.
      * If one of the sub-elements is not found, sets to null.
-     * @param element current RSS feed item
-     * @return ItemModel object that represents feed item
+     *
+     * @param       element current RSS feed item
+     * @return      ItemModel object that represents feed item
      */
     private ItemModel createItem(Element element) {
         String itemTitle = getOptionalSubElementValue(element, TITLE);
         String itemDescription = getOptionalSubElementValue(element, DESCRIPTION);
-        Date pubDate = DateUtils.parseDate(getOptionalSubElementValue(element, PUB_DATE)).orElse(null);
+        Date pubDate;
+        try {
+            pubDate = DateUtils.parseDateFromString(getOptionalSubElementValue(element, PUB_DATE));
+        } catch (InvalidPubDateException e) {
+            // log
+            pubDate = null;
+        }
 
         String mediaUrl = Optional.ofNullable(
                 element.getElementsByTagName(ENCLOSURE).item(0)
@@ -156,6 +197,13 @@ public class RssParser {
                 .build();
     }
 
+    /**
+     * Retrieves the text content of an optional sub-element within the provided XML element.
+     *
+     * @param element   XML element to search for the sub-element.
+     * @param tag       Tag name of the sub-element to retrieve the text content from.
+     * @return          Text content of the sub-element, or null if it is not found.
+     */
     private String getOptionalSubElementValue(Element element, String tag) {
         return Optional.ofNullable(element.getElementsByTagName(tag).item(0))
                 .map(Node::getTextContent).orElse(null);
