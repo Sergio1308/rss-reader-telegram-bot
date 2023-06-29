9bf0b57b457d54d9f2ca871aac23635ebf8ca4f5
Sergei <sergi.haiduk@gmail.com>
2023-06-29 23:33:11 +0300

    added throwing custom exceptions

--- src/main/java/com/company/RssReaderBot/utils/parser/ParseElements.java
+++ src/main/java/com/company/RssReaderBot/utils/parser/ParseElements.java
@@ -3,6 +3,7 @@ package com.company.RssReaderBot.utils.parser;
 import com.company.RssReaderBot.db.entities.RssFeed;
 import com.company.RssReaderBot.db.repositories.RssFeedRepository;
 import com.company.RssReaderBot.models.ItemsList;
+import com.company.RssReaderBot.utils.InvalidUserEnteredDateException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,7 +21,7 @@ public class ParseElements {
         this.itemsList = itemsList;
     }
 
-    public void findFeedAndParseRss(Integer feedId) {
+    public void findFeedAndParseRss(Integer feedId) throws RssParsingException {
         RssFeed feed = feedRepository.findById(feedId).orElseThrow();
         parser.setFeedId(feedId);
         parser.parseRss(feed.getUrl());
@@ -30,12 +31,12 @@ public class ParseElements {
         itemsList.setItemsList(parser.getElementListByTitle(title));
     }
 
-    public void parseAllElements(Integer feedId) {
+    public void parseAllElements(Integer feedId) throws RssParsingException {
         findFeedAndParseRss(feedId);
         itemsList.setItemsList(parser.getAllElementsList());
     }
 
-    public void parseElementsByDate(String dateString) {
+    public void parseElementsByDate(String dateString) throws InvalidUserEnteredDateException {
         itemsList.setItemsList(parser.getElementListByDate(dateString));
     }
 }
